f5aacf9f585316c256c28e1bb72480e86b7e1aaf
TomaNeshkov <toma.neshkov.highschool@svetlina.net>
2023-10-19 19:37:32 +0300

    Update Hangman.cs

--- HangmanByToma/HangmanByToma/Hangman.cs
+++ HangmanByToma/HangmanByToma/Hangman.cs
@@ -55,7 +55,6 @@ class Program
         Console.Clear();
         Console.WriteLine(wrongGuessesFrames[incorrectGuess]);
         Console.ForegroundColor = ConsoleColor.Green;
-        Console.ForegroundColor = ConsoleColor.DarkRed;
         Console.Write("Guess: ");
         Console.ForegroundColor = ConsoleColor.White;
         Console.Write($"{guess}");
